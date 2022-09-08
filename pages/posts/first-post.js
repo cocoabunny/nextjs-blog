7918042f4e00ed09fe98c54b13e5f59c2e6907bb
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-09-08 20:09:19 +1000

    Adding Link element and first page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Whamo () {
+    return(
+<>
+    <h1>Firste Post</h1>
+    <h2>
+        <Link href="/">Back to Home</Link>
+    </h2>
+</>
+    );
+}
